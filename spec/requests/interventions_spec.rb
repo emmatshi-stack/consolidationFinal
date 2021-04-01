68bd1eac56d7495ea1fc7b8d7fe028f316af2da2
emmatshi-stack <tshibangubiayi@gmail.com>
2021-03-31 23:23:19 -0400

    10exams

--- spec/requests/interventions_spec.rb
+++ spec/requests/interventions_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Interventions", type: :request do
   
   describe "GET #getbatteries" do
-        context "verifies that the buildings function in the intervention controller works correctly" do
+      context "verifies that the buildings function in the intervention controller works correctly" do
           it "creates a Widget" do
             headers = { "ACCEPT" => "text/html" }
             post '/interventions/getbatteries', :params => { :widget => {:name => "My Widget"} }, :headers => headers
@@ -11,9 +11,21 @@ RSpec.describe "Interventions", type: :request do
             expect(response.content_type).to eq("text/html")
             
           end
+      end
+
+      context "verifies that the buildings function in the intervention controller works correctly" do
+        it "creates a Widget" do
+          headers = { "ACCEPT" => "text/html" }
+          post '/interventions/getbuildings', :params => { :widget => {:name => "My Widget"} }, :headers => headers
+      
+          expect(response.content_type).to eq("text/html")
+          
         end
-            
     end
+            
+  end
+
+
     
   
 end
