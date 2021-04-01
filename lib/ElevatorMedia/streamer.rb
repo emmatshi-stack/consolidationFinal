8e5590711bfb0428c2a5d5f61a7ca550bead051b
emmatshi-stack <tshibangubiayi@gmail.com>
2021-04-01 12:34:55 -0400

    2test interventions

--- lib/ElevatorMedia/streamer.rb
+++ lib/ElevatorMedia/streamer.rb
@@ -6,12 +6,18 @@ require 'httparty'
 
 class Streamer
     def getContent()
-        response = HTTParty.get("https://apiconsolidation.azurewebsites.net/Elevators/2")
-        
+        response = HTTParty.get("https://ipinfo.io/161.185.160.93/geo")
         puts response
-        html_body="<div style='margin:0 auto; width:80%'><p>"+response['typeBuilding']+"</p>
-                            <p>"+response['serialNumber']+response['typeBuilding']+"</p>
+
+        if response['region'].is_a? String and response['country'].is_a? String and response['city'].is_a? String
+            html_body="<div style='margin:0 auto; width:80%'><p>"+response["city"]+"</p>
+                            <p>"+response['region']+response["country"]+"</p>
                             </div>"
+        else
+            html_body ="<div> there is a problem with the API try again later</div>"
+
+        end
+
     
         puts html_body
         #return html_body
--- spec/controllers/interventions_controller_spec.rb
+++ spec/controllers/interventions_controller_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 require 'json'
 
 describe InterventionsController, type: :controller do
-    # render_views
-    describe "GET #getbuildings" do
-        context "verifies that the buildings function in the intervention controller works correctly" do
-            it "returns that the function works successfully" do
+    
+    describe "checking on different function in the Intervention Controller when they are called" do
+        context "verifies that the getbuildings function in the intervention controller works correctly" do
+            it "Return buildings associated to a customer_id" do
                 params = {
                     "custormerid" => 1
                 }
                 get :getbuildings, params: params,  format: :json
-
                 res = JSON.parse(response.body).first
-                puts res
-                p  res["id"]
+                expect(res["id"]).to eq(1)
+            end
+        end
+        context "verifies that the getbatteries function in the intervention controller works correctly" do
+            it "Return batteries associated to a customer_id" do
+                params = {
+                    "buildingid" => 1
+                }
+                get :getbatteries, params: params,  format: :json
+                res = JSON.parse(response.body).first
                 expect(res["id"]).to eq(1)
             end
         end    
